1eb88bb7351080a647059a01e0aba7031c25ac44
jiesoul <jiesoul@gmail.com>
2018-01-25 16:30:52 +0800

    凯撒密码

--- src/clojurelearning/codewar.clj
+++ src/clojurelearning/codewar.clj
@@ -89,6 +89,12 @@
 (def step
   (memoize prime))
 
+(defn z-primes
+  ([] (concat (z-primes (range 2 Long/MAX_VALUE))))
+  ([coll]
+    (lazy-seq
+      (cons ))))
+
 (defn finance-1 [n]
   (loop [start 0
          end (+ n 1)
@@ -239,18 +245,29 @@
 (triangle-type 3 4 5) ; Right
 (triangle-type 7 12 8)
 
-(defn primess
-  []
-  )
+(def primes
+  (concat
+    [2 3 5 7]
+    (lazy-seq
+      (let [primes-from
+            (fn primes-from [n [f & r]]
+              (if (some #(zero? (rem n %))
+                        (take-while #(<= (* % %) n) primes))
+                (recur (+ n f) r)
+                (lazy-seq (cons n (primes-from (+ n f) r)))))
+            wheel (cycle [2 4 2 4 6 2 6 4 2 4 6 6 2 6 4 2
+                          6 4 6 8 4 2 4 2 4 8 6 4 6 2 4 6
+                          2 6 6 4 2 4 6 2 6 4 2 4 2 10 2 10])]
+        (primes-from 11 wheel)))))
 
 ;;Emirps
 (defn find-emirp
   [n]
   (letfn [(primes []
-            (lazy-seq (letfn [(step [coll]
-                       (let [head (first coll)]
-                         (lazy-cat [] [head] (step (filter #(pos? (mod % head)) coll)))))]
-               (step (range 2 Long/MAX_VALUE)))))
+             (letfn [(step [coll]
+                         (let [head (first coll)]
+                           (lazy-cat [head] (step (filter #(pos? (mod % head)) coll)))))]
+                 (step (range 2 Long/MAX_VALUE))))
           (primes? [n]
             (let [coll (range 2 (inc (Math/sqrt n)))]
               (not-any? #(zero? (rem n %)) coll)))
@@ -258,5 +275,13 @@
             (= n (last (take-while #(<= % n) (primes)))))
           (reverse-num [d]
             (Integer/parseInt (apply str (reverse (seq (str d))))))]
-    (let [xs (primes-n n)]
-      [(count xs), (apply max xs), (reduce + xs)])))
+    (let [d (reduce * (repeat (count (seq (str n))) 10))
+          xs (take-while #(<= % d) (primes))
+          xx (take-while #(<= % n) xs)
+          xs (filter #((reverse-num %)) xs)
+          xx (filter #(not= % (reverse-num %)) xx)]
+      xx)))
+
+
+;;First Variation on Caesar Cipher
+
