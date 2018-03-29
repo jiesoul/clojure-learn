8215355c1bfdd504a0c23ce18f121a0f50c881f7
jiesoul <jiesoul@gmail.com>
2018-03-30 01:17:43 +0800

    还是得看 sicp

--- src/clj/sicp/ch01.clj
+++ src/clj/sicp/ch01.clj
@@ -2,3 +2,33 @@
 
 (defn square [x]
   (* x x))
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn abs [n]
+  (max n (- n)))
+
+(defn good-enough? [guess x]
+  (< (abs (- (square guess) x)) 0.001))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(defn sqrt [x]
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
+(sqrt (+ 100 37))
+(square (sqrt 1000))
+
+(defn factorial [n]
+  (if (= n 1)
+    1
+    (* n (factorial (- n 1)))))
+
