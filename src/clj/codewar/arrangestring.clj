a086a0f680b6fbe047eb47b77752987edd9c2aca
jiesoul <jiesoul@gmail.com>
2018-04-02 17:44:13 +0800

    混乱的一天

---
+++ src/clj/codewar/arrangestring.clj
@@ -0,0 +1,12 @@
+(ns codewar.arrangestring)
+
+(defn step [simbol [first & rest]]
+  (cond
+    (empty? rest) [first]
+    (simbol first (first rest)) ))
+
+(defn arrange [strng]
+  (let [coll (clojure.string/split strng #" ")]
+    coll))
+
+(arrange "who hit retaining The That a we taken")
--- src/clj/sicp/ch01.clj
+++ src/clj/sicp/ch01.clj
@@ -44,3 +44,10 @@
 
 (factorial 1000)
 
+(defn cube [x] (* x x x))
+(defn p [x] (- (* 3 x) (* 4 (cube x))))
+(defn sine [angle]
+  (if (not (> (Math/abs angle) 0.1))
+    angle
+    (p (sine (/ angle 3.0)))))
+
