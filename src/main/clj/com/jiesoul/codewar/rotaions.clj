2c941a40ef3490165160a6708bf482fe192c3192
zhangyunjie <jiesoul@gmail.com>
2019-02-22 18:26:55 +0800

    goon

--- src/main/clj/com/jiesoul/codewar/rotaions.clj
+++ src/main/clj/com/jiesoul/codewar/rotaions.clj
@@ -1,9 +1,19 @@
 (ns com.jiesoul.codewar.rotaions)
 
+(defn raotain [s]
+  (let [c (count s)
+        n (quot c 2)
+        r (rem c 2)
+        s (vec s)]
+    (apply str (concat (subvec s (+ n r) c) (subvec s n (+ n r)) (subvec s 0 n)))))
+
 (defn contain-all-rots [strng arr]
   (if (= strng "")
     true
-    (let [f (fn [s]
-              (let [c (count s)]
-                c))]
-      (contains? (set arr) strng))))
+    (let [s1 (set arr)
+          s2 (raotain strng)]
+      (and (contains? s1 strng) (contains? s1 s2)))))
+
+(raotain "bsjq")
+(raotain "Ajylvpy")
+(contain-all-rots "Ajylvpy" ["Ajylvpy", "ylvpyAj", "jylvpyA", "lvpyAjy", "pyAjylv", "vpyAjyl", "ipywee"])
