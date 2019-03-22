162fe93dfb8f971de26648a89798233987bc2bd2
zhangyunjie <jiesoul@gmail.com>
2019-03-22 17:38:27 +0800

    sicp

--- src/main/clj/com/jiesoul/sicp/ex02.clj
+++ src/main/clj/com/jiesoul/sicp/ex02.clj
@@ -20,14 +20,39 @@
 (defn y-point [point]
   (second point))
 
-(defn make-segment []
-  )
+(defn make-segment [p1 p2]
+  [p1 p2])
 
-(defn start-segment []
-  )
+(defn start-segment [s]
+  (first s))
 
-(defn end-segment []
-  )
+(defn end-segment [s]
+  (second s))
+
+(defn print-point [p]
+  (str "(" (x-point p) "," (y-point p) ")"))
+
+(defn midpoint-segment [s]
+  (let [p1   (start-segment s)
+        p2   (end-segment s)
+        x1   (x-point p1)
+        x2   (x-point p2)
+        y1   (y-point p1)
+        y2   (y-point p2)
+        step (fn [v1 v2]
+               (let [mi (min v1 v2)
+                     ma (max v1 v2)]
+                 (+ mi (/ (- ma mi) 2))))]
+    [(step x1 x2) (step y1 y2)]))
+
+;; 2.3
+(defn make-rectangle [segment]
+  (let [p1 (start-segment segment)
+        p2 (end-segment segment)]
+    [p1 (make-point (x-point p1) (y-point p2))
+     p2 (make-point (x-point p2) (y-point p1))]))
+
+(defn len-rectangle [rectangle])
 
 (defn make-interval [a b]
   [a b])
@@ -54,3 +79,21 @@
   (mul-interval x
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
+
+
+(defn list-ref [items n]
+  (if (zero? n)
+    (first items)
+    (recur (rest items) (dec n))))
+
+(defn length [items]
+  (if (nil? items)
+    0
+    (+ 1 (length (rest items)))))
+
+(defn length-iter [items]
+  (let [step (fn [a count]
+               (if (nil? a)
+                 count
+                 (recur (rest a) (inc count))))]
+    (step items 0)))
--- test/main/clj/com/jiesoul/sicp/ex02_test.clj
+++ test/main/clj/com/jiesoul/sicp/ex02_test.clj
@@ -1,6 +1,6 @@
 (ns com.jiesoul.sicp.ex02-test
   (:require [clojure.test :refer :all])
-  (:require [com.jiesoul.sicp.ex02 :refer [make-rat-real]]))
+  (:require [com.jiesoul.sicp.ex02 :refer :all]))
 
 (deftest make-rat-real-test
   (is (= (make-rat-real -1 2) [-1 2]))
@@ -9,3 +9,16 @@
   (is (= (make-rat-real 1 2) [1 2]))
 
   )
+
+(deftest midpoint-segment-test
+  (testing "midpoint-segment-test"
+    (is (= (midpoint-segment (make-segment (make-point 1 1) (make-point -1 -1))) [0 0]))
+    (is (= (midpoint-segment (make-segment (make-point 2 1) (make-point -2 -1))) [0 0]))
+    (is (= (midpoint-segment (make-segment (make-point 1 1) (make-point 3 3))) [2 2]))
+    ))
+
+(deftest make-rectangle-test
+  (testing "sss"
+    (is (= (make-rectangle (make-segment (make-point 1 1) (make-point -1 -1)))
+           [[1 1] [1 -1] [-1 -1] [-1 1]]))
+    ))
